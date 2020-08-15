5063a5f5f5f59b916a69179e3064d236a7c3cc48
Kishore Chakkirala <kishore.chakkirala@yahoo.com>
2020-08-16 00:28:43 +0530

    Added a new class

---
+++ Tailspin.SpaceGame.Web/Sample.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tailspin.SpaceGame.Web
+{
+    public class Sample
+    {
+    }
+}
